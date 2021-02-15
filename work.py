545ca6edec7b43e746fe2bfb0732d0917e066e77
ojo michael <74685782+MY-KHEL@users.noreply.github.com>
2021-02-15 20:39:22 +0100

    Add files via upload

--- work.py
+++ work.py
@@ -1,4 +1,4 @@
-customerName = input('Please input your Name: ')
+customerName = input('Please input your Name: ').capitalize()
 print()
 
 #Beginning of the Phone Number Session
@@ -34,51 +34,54 @@ while('@gmail.com' or '@hotmail.com' or '@yahoo.com') not in customerEmail:
 print('_'*40)
 print()
 
-storeItems = {'rice':200,'bread':200,'egg':70,'beans':100,'indomie':40}
-print('This is what we have in store , please select from the items below \n', tuple(storeItems))
+item = {'rice':240,'beans':120,'egg':50,'biscuit':10,'beef':400,'drink':100}
+print('Welcome to Mykhel Stores')
 print()
+print('This is what we have in store and their prices. Choose your order from the items below')
 
 
-itemName = (input('The name of the item you are buying ').lower())
-itemQuantity = eval(input('What is the quantiy of what you are buying '))
-print()
-priceForOne = (storeItems[itemName])
-tPriceOfProduct = itemQuantity * priceForOne
-print('Price for One : ',priceForOne)
-print('The total Price of the Product : ',tPriceOfProduct)
-    
-itemName5 = (input('The name of the item you are buying ').lower())
-itemQuantity5 = eval(input('What is the quantiy of what you are buying '))
-print()
-priceForOne5 = (storeItems[itemName5])
-tPriceOfProduct = int(itemQuantity5 * priceForOne5)
-print('Price for One : ',priceForOne5)
-print('The total Price of the Product : ',tPriceOfProduct5)
-    
-itemName2 = (input('The name of the item you are buying ').lower())
-itemQuantity2 = eval(input('What is the quantiy of what you are buying '))
-print()
-priceForOne2 = (storeItems[itemName2])
-tPriceOfProduct2 = int(itemQuantity2 * priceForOne2)
-print('Price for One : ',priceForOne2)
-print('The total Price of the Product : ',tPriceOfProduct2)
-    
-itemName3 = (input('The name of the item you are buying ').lower())
-itemQuantity3 = eval(input('What is the quantiy of what you are buying '))
-print()
-priceForOne3 = (storeItems[itemName3])
-tPriceOfProduct3 = int(itemQuantity3 * priceForOne3)
-print('Price for One : ',priceForOne3)
-print('The total Price of the Product : ',tPriceOfProduct3)
-    
-itemName4 = (input('The name of the item you are buying ').lower())
-itemQuantity4 = eval(input('What is the quantiy of what you are buying '))
-print()
-priceForOne4 = (storeItems[itemName4])
-tPriceOfProduct4 = int(itemQuantity4 * priceForOne4)
-print('Price for One : ',priceForOne4)
-print('The total Price of the Product : ',tPriceOfProduct4)
+print(item)
+
     
 
     
-print('Your Total price is ',tPriceOfProduct+tPriceOfProduct2+tPriceOfProduct3+tPriceOfProduct4+tPriceOfProduct5)
+n = int(input('\n How many Goods are you buying: '))
+
+purchase = {}
+for i in range(n):
+    itemName = input('Enter your Order \n').lower()
+    while itemName not in item.keys():
+        itemName = input('We dont have that in store, check the Store for items we have \n').lower()
+        print()
+    itemQuantity = int(input('How many of this product are you buying: '))
+    itemPrice = item[itemName] 
+    totalPrice = str(itemPrice*itemQuantity)
+    print('Price for one : ',itemPrice)
+    print('Price for ',itemQuantity,': ',totalPrice)
+    
+    purchase[itemName]=itemQuantity 
+    
+    i += 1
+    
+totalValue = {}
+print()
+print('_'*100)
+print()
+print('Get your Invoice')
+print()
+print('Name: ',customerName)
+print()
+print('Phone Number: ',customerPhone)
+print()
+print('Email: ',customerEmail)
+print()
+print('Keep shopping at Mykhel Stores')
+print('_'*40)
+print('Item',' '*9,'Qty',' '*7, 'Total')
+print()
+for key, value in purchase.items():
+    print(key, '-' * 10, value, '-' * 10, value * item.get(key))
+    totalValue[key] = value * item.get(key)
+print(' ' * 40)
+print('Total', '-' * 22, sum(total_value.values()))
+print('\n Thank you for Shopping with us!')
